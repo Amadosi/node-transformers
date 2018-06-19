f169c463f9277bade3afb92337ab09ab3cb4ac5a
Amadosi <odaiboamadosi@gmail.com>
2018-06-19 13:22:12 +0100

    initials project setup

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+	const AbstractTransformer = require("./src/AbstractTransformer");
+
+    module.exports = {
+    	AbstractTransformer
+    }
---
+++ src/AbstractTransformer.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const key = process.env.TRANSFORMER_DATA_KEY || 'data';
+
+class Transformer {
+
+	/* *
+	 * constructor taking an orm 
+	 * object 
+	 * */
+    constructor(orm){
+        this._orm = orm;
+    }
+
+	/**
+	 * overridable transform function 
+	 * to get the users custom transform 
+	 **/
+    transform(orm){
+    	//defaultly return the json format 
+    	//of the orm object
+        return this._orm.toJson();
+    };
+
+	/**
+	 * function to get the serialised version
+	 * of the transformer 
+	 **/
+    toJson(){
+    	//returns the serialised version based on the 
+    	//user defined key of the data key
+      return {[key]:this.transform(this._orm)};
+    };
+}
+
+module.exports = Transformer;
